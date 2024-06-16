252f728af3b874d4bcd0022d3784c2f7fcf9dd2e
Kingdk88 <143623116+Kingdk88@users.noreply.github.com>
2024-06-16 07:57:13 +0530

    Update approve.py

--- PURVIMUSIC/plugins/bot/approve.py
+++ PURVIMUSIC/plugins/bot/approve.py
@@ -104,7 +104,7 @@ async def autoapprove(client: app, message: ChatJoinRequest):
                 [
                     [
                         InlineKeyboardButton(
-                            " ๏ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ๏ ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+                            " ๏ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ๏ ", url=f"https://t.me/Sukku_music_bot?startgroup=true")
                     ]
                 ]
             ),
